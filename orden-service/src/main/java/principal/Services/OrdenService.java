2d33147dc280ab39246e80d62f8b80cf4f986799
rusok <rusok.software@gmail.com>
2026-03-19 20:00:52 -0400

    Servidor de configuraciones centralizado

--- orden-service/src/main/java/principal/Services/OrdenService.java
+++ orden-service/src/main/java/principal/Services/OrdenService.java
@@ -1,8 +1,10 @@
 package principal.Services;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import principal.Configs.ProductoClient;
 import principal.Models.Orden;
 import principal.Models.Producto;
 import principal.Repositories.OrdenRepository;
@@ -10,15 +12,21 @@ import principal.Repositories.OrdenRepository;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class OrdenService {
     @Autowired
     private OrdenRepository ordenRepository;
     @Autowired
     private RestTemplate restTemplate;
+    private final ProductoClient productoClient;
     public Orden registrarOrden(Orden orden){
         // Llamada al microservicio de producto-service
-        String productoUrl = "http://localhost:8081/api/v1/productos/"+orden.getProductoId();
-        Producto producto = restTemplate.getForObject(productoUrl, Producto.class);
+        //String productoUrl = "http://localhost:8081/api/v1/productos/"+orden.getProductoId();
+        //Producto producto = restTemplate.getForObject(productoUrl, Producto.class);
+
+        //Usando feign y Eureka Server
+        Producto producto = productoClient.obtenerProducto(orden.getProductoId());
+
         if(producto == null) throw new RuntimeException("Producto no encontrado");
         orden.setPrecioTotal(producto.getPrecio() * orden.getCantidad());
         return ordenRepository.save(orden);
